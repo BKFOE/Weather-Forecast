1984a3118d5c9b5a220755b214389dd1d898e45d
bkfoe <tirrell.barronette@gmail.com>
2023-09-06 14:21:01 -0400

    Add widgets and plot fake data dynamically

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forecast for Next 5 Days")
 place = st.text_input("Location: ")
@@ -6,3 +7,16 @@ day = st.slider("Forecast Days:", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Selext data to view",
                       ("Temperature", "Weather"))
 st.subheader(f"{option} for the next {day} in {place}")
+
+
+def get_data(day):
+    dates = ["2023-09-06", "2023-09-07", "2023-09-08"]
+    temperatures = [24, 25, 26]
+    temperatures = [day * i for i in temperatures]
+    return dates, temperatures
+
+
+d, t = get_data(day)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
